9dfe93ea7018473dd06ba6fa8bb62ee335061a2c
chefirchy <42928834+chefirchy@users.noreply.github.com>
2020-10-25 22:20:19 +0200

    Add files via upload

---
+++ l1p1/src/l1p1/math.java
@@ -0,0 +1,15 @@
+package l1p1;
+
+public class math {
+    public static void main(String args[]) {
+      int a=10;
+      int b=25;
+      double x=Math.pow(a, 3);
+      double y=Math.pow(b, 3);
+      double z = ((x+y)/2);
+      double y1 = x*y;
+      double c = Math.sqrt(y1);
+      System.out.println("serarif = " + z);
+      System.out.println("sergeom = " + c);
+    }
+}
